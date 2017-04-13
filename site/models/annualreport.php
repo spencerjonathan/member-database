6667b261b4f6da0ed7bf874f151f845423879ab8
Jon Spencer <spencer.jonathan@gmail.com>
2017-04-13 21:34:25 +0100

    Got manager privilages working

--- site/models/annualreport.php
+++ site/models/annualreport.php
@@ -7,94 +7,93 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 // No direct access to this file
-defined('_JEXEC') or die('Restricted access');
- 
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
+
 /**
  * MemberDatabaseList Model
  *
- * @since  0.0.1
+ * @since 0.0.1
  */
-class MemberDatabaseModelAnnualreport extends JModelList
-{
+class MemberDatabaseModelAnnualreport extends JModelList {
 	/**
 	 * Constructor.
 	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see     JController
-	 * @since   1.6
+	 * @param array $config
+	 *        	An optional associative array of configuration settings.
+	 *        	
+	 * @see JController
+	 * @since 1.6
 	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
+	public function __construct($config = array()) {
+		parent::__construct ( $config );
 	}
-
+	
 	/**
 	 * Method to build an SQL query to load the list data.
 	 *
-	 * @return      string  An SQL query
+	 * @return string An SQL query
 	 */
-
-	public function getMembers()
-	{
+	public function getMembers() {
 		// Initialize variables.
-		$db    = JFactory::getDbo();
-		$userid = JFactory::getUser()->id;
-		$query = $db->getQuery(true);
- 
+		$db = JFactory::getDbo ();
+		$userid = JFactory::getUser ()->id;
+		$query = $db->getQuery ( true );
+		
 		// Create the base select statement.
-		$query->select('m.tower_id, concat_ws(\', \',surname, forenames) as name')
-                ->from($db->quoteName('#__md_member', 'm'))
-		->join('INNER', $db->quoteName('#__md_usertower', 'ut') . ' ON (' . $db->quoteName('m.tower_id') . ' = ' . $db->quoteName('ut.tower_id') . ')')
-		->where('ut.user_id = ' . $userid)
-		->order('surname, forenames asc');
-
-		$db->setQuery($query);
-
-		$results = $db->loadObjectList();
-
+		$query->select ( 'm.tower_id, concat_ws(\', \',surname, forenames) as name' )->from ( $db->quoteName ( '#__md_member', 'm' ) );
+		
+		if (! JFactory::getUser ()->authorise ( 'core.manage', 'com_memberdatabase' )) {
+			$query->join ( 'INNER', $db->quoteName ( '#__md_usertower', 'ut' ) . ' ON (' . $db->quoteName ( 'm.tower_id' ) . ' = ' . $db->quoteName ( 'ut.tower_id' ) . ')' );
+			$query->where ( 'ut.user_id = ' . $userid );
+		}
+		
+		
+		$query->order ( 'surname, forenames asc' );
+		
+		$db->setQuery ( $query );
+		
+		$results = $db->loadObjectList ();
+		
 		return $results;
 	}
-
-	public function getDistricts()
-	{
+	public function getDistricts() {
 		// Initialize variables.
-		$db    = JFactory::getDbo();
-		$userid = JFactory::getUser()->id;
-		$query = $db->getQuery(true);
- 
+		$db = JFactory::getDbo ();
+		$userid = JFactory::getUser ()->id;
+		$query = $db->getQuery ( true );
+		
 		// Create the base select statement.
-		$query->select('d.*')
-                ->from($db->quoteName('#__md_district', 'd'))
-		->order('id asc');
-
-		$db->setQuery($query);
-
-		$results = $db->loadAssocList('id', 'name');
-
+		$query->select ( 'd.*' )->from ( $db->quoteName ( '#__md_district', 'd' ) )->order ( 'id asc' );
+		
+		$db->setQuery ( $query );
+		
+		$results = $db->loadAssocList ( 'id', 'name' );
+		
 		return $results;
 	}
-
-	public function getTowers()
-	{
+	public function getTowers() {
 		// Initialize variables.
-		$db    = JFactory::getDbo();
-		$userid = JFactory::getUser()->id;
-		$query = $db->getQuery(true);
- 
+		$db = JFactory::getDbo ();
+		$userid = JFactory::getUser ()->id;
+		$query = $db->getQuery ( true );
+		
 		// Create the base select statement.
-		$query->select('t.*, cor.title as corresp_title, cor.surname as corresp_surname, cor.forenames as corresp_forenames, cor.telephone as corresp_telephone, cap.title as captain_title, cap.surname as captain_surname, cap.forenames as captain_forenames, cap.telephone as captain_telephone') 
-                ->from($db->quoteName('#__md_tower', 't'))
-		->join('INNER', $db->quoteName('#__md_usertower', 'ut') . ' ON (' . $db->quoteName('t.id') . ' = ' . $db->quoteName('ut.tower_id') . ')')
-		->join('LEFT', $db->quoteName('#__md_member', 'cor') . ' ON (' . $db->quoteName('t.correspondent_id') . ' = ' . $db->quoteName('cor.id') . ')')
-		->join('LEFT', $db->quoteName('#__md_member', 'cap') . ' ON (' . $db->quoteName('t.captain_id') . ' = ' . $db->quoteName('cap.id') . ')')
-		->where('ut.user_id = ' . $userid)
-		->order('district_id, place, designation asc');
-
-		$db->setQuery($query);
-
-		$results = $db->loadObjectList();
-
+		$query->select ( 't.*, cor.title as corresp_title, cor.surname as corresp_surname, cor.forenames as corresp_forenames, cor.telephone as corresp_telephone, cap.title as captain_title, cap.surname as captain_surname, cap.forenames as captain_forenames, cap.telephone as captain_telephone' );
+		$query->from ( $db->quoteName ( '#__md_tower', 't' ) );
+		$query->join ( 'LEFT', $db->quoteName ( '#__md_member', 'cor' ) . ' ON (' . $db->quoteName ( 't.correspondent_id' ) . ' = ' . $db->quoteName ( 'cor.id' ) . ')' );
+		$query->join ( 'LEFT', $db->quoteName ( '#__md_member', 'cap' ) . ' ON (' . $db->quoteName ( 't.captain_id' ) . ' = ' . $db->quoteName ( 'cap.id' ) . ')' );
+		
+		if (! JFactory::getUser ()->authorise ( 'core.manage', 'com_memberdatabase' )) {
+			$query->join ( 'INNER', $db->quoteName ( '#__md_usertower', 'ut' ) . ' ON (' . $db->quoteName ( 't.id' ) . ' = ' . $db->quoteName ( 'ut.tower_id' ) . ')' );
+			$query->where ( 'ut.user_id = ' . $userid );
+		}
+		
+		$query->order ( 'district_id, place, designation asc' );
+		
+		$db->setQuery ( $query );
+		
+		$results = $db->loadObjectList ();
+		
 		return $results;
 	}
 }
--- site/views/annualreport/tmpl/default.php
+++ site/views/annualreport/tmpl/default.php
@@ -4,9 +4,9 @@
  * @subpackage  com_memberdatabase
  *
  */
- 
+
 // No direct access to this file
-defined('_JEXEC') or die('Restricted Access');
+defined ( '_JEXEC' ) or die ( 'Restricted Access' );
 
 ?>
 
@@ -15,38 +15,45 @@ defined('_JEXEC') or die('Restricted Access');
 	<div class="row-fluid">
 		<div class="span12">
 
-	<?php $currentdistrictid = 0; $firstdistrict=1;
-	foreach ($this->towers as $tower) : 
-		if ($currentdistrictid != $tower->district_id) : $currentdistrictid = $tower->district_id; ?>
-			<h1 <?php if ($firstdistrict) { echo 'class="firstdistrict"'; $firstdistrict=0; } ?> align="center"><?php echo $this->districts[$tower->district_id] ?></h1>
+	<?php
+	
+	$currentdistrictid = 0;
+	$firstdistrict = 1;
+	foreach ( $this->towers as $tower ) :
+		if ($currentdistrictid != $tower->district_id) :
+			$currentdistrictid = $tower->district_id;
+			?>
+			<h1
+				<?php if ($firstdistrict) { echo 'class="firstdistrict"'; $firstdistrict=0; } ?>
+				align="center"><?php echo $this->districts[$tower->district_id] ?></h1>
 			<h2 align="center">TOWER DETAILS AND MEMBERS FOR 2017</h2>
 		<?php endif ?>
 	<div class="tower">
-	<table style="table-layout: fixed;" width="100%">
-		<col width="29%"/>
-		<col width="5%"/>
-		<col width="17%"/>
-		<col width="17%"/>
-		<col width="17%"/>
-		<col width="15%"/>
-		<tbody>
-		<tr>
-			<td><strong><?php echo $tower->place ?>, <?php echo $tower->designation ?></strong></td>
-			<td align="center"><?php echo $tower->bells ?></td>
-			<td align="centre"><?php echo $tower->tenor ?></td>
-			<td align="centre"><?php if ($tower->ground_floor) { echo 'GF'; } ?></td>
-			<td COLSPAN=2 align="right">OS: <?php echo $tower->grid_ref . ' / ' . $tower->post_code ?></td>
-		</tr>
-		<tr>
-			<td COLSPAN=2>Practice: <?php echo $tower->practice_night . ' ' . $tower->practice_details?></td>
-			<td COLSPAN=4 align="right">Sunday: <?php echo $tower->sunday_ringing ?></td>
-		</tr>
-		<tr>
-			<td COLSPAN=6>Corresp: <?php echo $tower->corresp_title . ' ' . substr($tower->corresp_forenames, 0, 1) . ' ' . $tower->corresp_surname . ', ' . $tower->corresp_telephone . ' - ' . $tower->email ?></td>
-		</tr>
-		<tr>
-			<td COLSPAN=6>Captain: <?php echo $tower->captain_title . ' ' . substr($tower->captain_forenames, 0, 1) . ' ' . $tower->captain_surname . ', ' . $tower->captain_telephone ?></td>
-		</tr>
+				<table style="table-layout: fixed;" width="100%">
+					<col width="29%" />
+					<col width="5%" />
+					<col width="17%" />
+					<col width="17%" />
+					<col width="17%" />
+					<col width="15%" />
+					<tbody>
+						<tr>
+							<td><strong><?php echo $tower->place ?>, <?php echo $tower->designation ?></strong></td>
+							<td align="center"><?php echo $tower->bells ?></td>
+							<td align="centre"><?php echo $tower->tenor ?></td>
+							<td align="centre"><?php if ($tower->ground_floor) { echo 'GF'; } ?></td>
+							<td COLSPAN=2 align="right">OS: <?php echo $tower->grid_ref . ' / ' . $tower->post_code ?></td>
+						</tr>
+						<tr>
+							<td COLSPAN=2>Practice: <?php echo $tower->practice_night . ' ' . $tower->practice_details?></td>
+							<td COLSPAN=4 align="right">Sunday: <?php echo $tower->sunday_ringing ?></td>
+						</tr>
+						<tr>
+							<td COLSPAN=6>Corresp: <?php echo $tower->corresp_title . ' ' . substr($tower->corresp_forenames, 0, 1) . ' ' . $tower->corresp_surname . ', ' . $tower->corresp_telephone . ' - ' . $tower->email ?></td>
+						</tr>
+						<tr>
+							<td COLSPAN=6>Captain: <?php echo $tower->captain_title . ' ' . substr($tower->captain_forenames, 0, 1) . ' ' . $tower->captain_surname . ', ' . $tower->captain_telephone ?></td>
+						</tr>
 		<?php $i = 1; foreach ($this->members as $member) : ?>
 			<?php if ($member->tower_id == $tower->id) : ?>
 				<?php if ($member->tower_id == $tower->id) : ?>
@@ -59,13 +66,20 @@ defined('_JEXEC') or die('Restricted Access');
 					<?php else : $i = $i + 1; endif ?>
 				<?php endif ?>
 			<?php endif ?>
-		<?php endforeach; 
-		if ($i != 1) : ?>
+		<?php
+				
+			
+		endforeach
+		;
+		if ($i != 1) :
+			?>
 			</tr> 
 		<?php endif ?>
 		</tbody>
-	</table>
-	</div>
+				</table>
+			</div>
 	<?php endforeach; ?>
 
-</div></div></div>
+</div>
+	</div>
+</div>

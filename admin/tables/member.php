cfd5b42fd9a951f0457732f73672cd6b8636336a
Jon Spencer <spencer.jonathan@gmail.com>
2017-06-06 20:44:11 +0100

    Added recording of mod_user and mod_date

--- admin/tables/member.php
+++ admin/tables/member.php
@@ -25,4 +25,13 @@ class MemberDatabaseTableMember extends JTable
 	{
 		parent::__construct('#__md_member', 'id', $db);
 	}
+	
+	public function store($updateNulls = false)
+	{
+		$this->mod_user_id = JFactory::getUser ()->id;
+		$this->mod_date = $currentDate = date('Y-m-d H:i:s');
+		
+		return parent::store($updateNulls);
+	}
 }
+?>

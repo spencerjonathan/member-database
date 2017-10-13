31decbeb36bc52c7ab20e32a5d1ce24d27f0a2b7
Jon Spencer <spencer.jonathan@gmail.com>
2017-10-13 22:19:18 +0100

    Added ability to specify since date on Member Status Change report

--- site/views/controlreport/tmpl/default.php
+++ site/views/controlreport/tmpl/default.php
@@ -11,32 +11,46 @@
 // No direct access to this file
 defined ( '_JEXEC' ) or die ( 'Restricted Access' );
 
-$this->items = $this->getModel("Members")->getMemberTypeChanges("2017-01-01");
+$jinput = JFactory::getApplication ()->input;
+$this->since = $jinput->get ( 'since', '2017-01-01' );
+
+$this->items = $this->getModel ( "Members" )->getMemberTypeChanges ( $this->since );
 
 ?>
 
-	<table class="table table-hover">
-		<thead>
-			<tr>
+
+<form action="">
+	<div class="form-horizontal">
+		<div class="control-label">Since</div>
+		<div class="controls">
+			<input type="date" name="since" value="<?php echo $this->since; ?>"> 
+			<input type="submit" class="btn btn-small btn-default" value="Update">
+		</div>
+	</div>
+</form>
+
+<table class="table table-hover">
+	<thead>
+		<tr>
 			<th>Name</th>
 			<th>Previous Type</th>
 			<th>New Type</th>
 			<th>Mod User</th>
 			<th>Mod Date</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php 
-			foreach ( $this->items as $row ) :
-					$link = JRoute::_ ( 'index.php?option=com_memberdatabase&view=memberhistory&memberId=' . $row->id );
-					?>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		foreach ( $this->items as $row ) :
+			$link = JRoute::_ ( 'index.php?option=com_memberdatabase&view=memberhistory&memberId=' . $row->id );
+			?>
 					<tr>
-				<td> <?php echo "<a href='$link'>$row->surname, $row->forenames</a>"; ?> </td>
-				<td> <?php echo $row->prev_member_type; ?> </td>
-				<td> <?php echo $row->member_type; ?> </td>
-				<td> <?php echo "$row->mod_name($row->mod_username)"; ?> </td>
-				<td> <?php echo $row->mod_date; ?> </td>
-			</tr>
+			<td> <?php echo "<a href='$link'>$row->surname, $row->forenames</a>"; ?> </td>
+			<td> <?php echo $row->prev_member_type; ?> </td>
+			<td> <?php echo $row->member_type; ?> </td>
+			<td> <?php echo "$row->mod_name($row->mod_username)"; ?> </td>
+			<td> <?php echo $row->mod_date; ?> </td>
+		</tr>
 				<?php endforeach; ?>
 		</tbody>
-	</table>
+</table>

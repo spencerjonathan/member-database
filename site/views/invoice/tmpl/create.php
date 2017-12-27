3e548884f4d3de228203972c8cb217cf7e078a00
Jon Spencer <spencer.jonathan@gmail.com>
2017-12-27 21:51:32 +0000

    Fixed cancel button on Invoice create screen, and made save invoice button more obvious

--- site/views/invoice/tmpl/create.php
+++ site/views/invoice/tmpl/create.php
@@ -8,8 +8,8 @@
 // No direct access to this file
 defined ( '_JEXEC' ) or die ( 'Restricted Access' );
 
-$this->members = $this->get('Members');
-$this->tower = $this->get('Tower');
+$this->members = $this->get ( 'Members' );
+$this->tower = $this->get ( 'Tower' );
 
 $document = JFactory::getDocument ();
 $document->addScriptDeclaration ( '
@@ -42,28 +42,22 @@ $document->addScriptDeclaration ( '
 	<div class="row-fluid">
 		<div class="span12">
 
-<button onclick="Joomla.submitbutton('invoice.add')" id="add_button"
-	class="btn btn-small btn-success">
-	<span class="icon-new icon-white"></span> Add & Close
-</button>
-<button onclick="Joomla.submitbutton('invoice.cancel')"
-	class="btn btn-small">
-	<span class="icon-cancel"></span> Close
-</button>
-<!--  <button onclick="var elements = document.getElementsByClassName('excl-checkbox'); Array.prototype.forEach.call(elements, function (element) { element.disabled = false; } );"
-	class="btn btn-small">
-	<span class="icon-unlock"></span> Exclude Some Members
-</button>   -->
+			<button onclick="Joomla.submitbutton('invoice.cancel')"
+				class="btn btn-small">
+				<span class="icon-cancel"></span> Close
+			</button>
 
-<hr>
-<h1> <?php echo $this->year; ?> Invoice for <?php echo $this->tower->name; ?></h1>
-Want to include a new member on the invoice? 
-<a
-		href="<?php echo JRoute::_('index.php/component/memberdatabase/?view=member&layout=edit&list_view=invoice'); ?>"
-		class="btn btn-success"><span	class="icon-new icon-white"></span> Add New Member</a><br>
-Remember, if you add a new member you also need to complete a New Member Nomination form and sent it to the Membership Coordinator.
+			<hr>
+			<h1> <?php echo $this->year; ?> Invoice for <?php echo $this->tower->name; ?></h1>
+			Want to include a new member on the invoice? <a
+				href="<?php echo JRoute::_('index.php/component/memberdatabase/?view=member&layout=edit&list_view=invoice'); ?>"
+				class="btn btn-small btn-success"><span class="icon-new icon-white"></span>
+				Add New Member</a><br> Remember, if you add a new member you also
+			need to complete a New Member Nomination form and sent it to the
+			Membership Coordinator.
 
-			<form class="adminform" name="adminForm" id="adminForm" method="post" action="<?php echo JRoute::_('index.php?option=com_memberdatabase&view=invoice&layout=create&towerId=' . (int) $this->tower->id); ?>">
+			<form class="adminform" name="adminForm" id="adminForm" method="post"
+				action="<?php echo JRoute::_('index.php?option=com_memberdatabase&view=invoice&layout=create&towerId=' . (int) $this->tower->id); ?>">
 
 				<table class="table table-bordered" width="100%">
 					<tr>
@@ -74,36 +68,50 @@ Remember, if you add a new member you also need to complete a New Member Nominat
 					</tr>
 			
 			<?php
-			$members = $this->get('Members');
+			$members = $this->get ( 'Members' );
 			$total_fee = 0.0;
 			
 			$id = 1;
 			
 			foreach ( $members as $member ) :
 				$total_fee = $total_fee + $member->fee;
-				$link = JRoute::_ ( 'index.php?option=com_memberdatabase&task=member.edit&list_view=invoice&id=' . $member->id);
-			
+				$link = JRoute::_ ( 'index.php?option=com_memberdatabase&task=member.edit&list_view=invoice&id=' . $member->id );
+				
 				?>
 					<tr>
-						<td><input id="checkbox-<?php echo $id; ?>" fee="<?php echo $member->fee; ?>" onchange="recalculate(<?php echo $id; ?>)" type="checkbox" class="excl-checkbox" name="cid[]" value="<?php echo $member->id; ?>" checked></td>
+						<td><input id="checkbox-<?php echo $id; ?>"
+							fee="<?php echo $member->fee; ?>"
+							onchange="recalculate(<?php echo $id; ?>)" type="checkbox"
+							class="excl-checkbox" name="cid[]"
+							value="<?php echo $member->id; ?>" checked></td>
 						<td><a href="<?php echo $link; ?>"
-					title="<?php echo JText::_('Edit Member'); ?>">
+							title="<?php echo JText::_('Edit Member'); ?>">
 							<?php echo $member->name; ?></a></td>
 						<td><?php echo $member->member_type; ?></td>
 						<td id="fee-<?php echo $id; ?>" style="text-align: right"><?php echo number_format((float)$member->fee, 2, '.', ''); ?></td>
 					</tr>
 
-			<?php 
-			$id = $id + 1; 
-			endforeach; ?>
+			<?php
+				$id = $id + 1;
+				endforeach;
+			?>
 					<tr>
-						<td colspan=4 id="total" fee="<?php echo $total_fee; ?>" style="text-align: right"><?php echo number_format((float)$total_fee, 2, '.', ''); ?></td>
+						<td colspan=4 id="total" fee="<?php echo $total_fee; ?>"
+							style="text-align: right"><?php echo number_format((float)$total_fee, 2, '.', ''); ?></td>
 					</tr>
 				</table>
-				<input type="hidden" name="task" value="invoice.add" />
-				<input type="hidden" name="tower-id" value="<?php echo $this->tower->id; ?>" />
-				<input type="hidden" name="year" value="<?php echo $this->year; ?>" />
+				<input type="hidden" name="task" value="invoice.add" /> <input
+					type="hidden" name="tower-id"
+					value="<?php echo $this->tower->id; ?>" /> <input type="hidden"
+					name="year" value="<?php echo $this->year; ?>" />
+					<?php echo JHtml::_('form.token'); ?>
 			</form>
+			<hr>
+			When you're satisfied that the draft invoice above is correct, click here on the "Save Invoice & Close" button to save it and get your invoice number: 
+			<button onclick="Joomla.submitbutton('invoice.add')" id="add_button"
+				class="btn btn-success">
+				<span class="icon-new icon-white"></span> Save Invoice & Close
+			</button>
 		</div>
 	</div>
 </div>

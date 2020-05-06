5deb5ad6c453db5daf7e2ac3dff0b6302e2d4fa2
Jon Spencer <membership@scacr.org>
2020-05-06 22:30:16 +0100

    Final fixes to the voting form

---
+++ site/views/election/tmpl/alreadysubmitted.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.core');
+JHtml::_('formbehavior.chosen', 'select');
+
+//JFactory::getDocument()->addScriptDeclaration($script);
+?>
+
+<legend>Annual Elections Submitted</legend>
+
+<hr>
+<h1>You have already submitted your vote!</h1>
+	
